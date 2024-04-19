c4c5c2b0e6c46c637a6ff65a97feb5d59e7aa582
Stephan Meijer <stephan.meijer@gmail.com>
2024-04-19 10:44:23 +0200

    chore: fix test

--- src/index.test.js
+++ src/index.test.js
@@ -1,1 +1,6 @@
-require('./rules/explicit-internal-boundaries.test.js');
+const rules = require('./index.js').rules;
+
+for (const rule of Object.keys(rules)) {
+  console.log(`testing: ${rule}`);
+  require(`./rules/${rule}.test.js`);
+}
